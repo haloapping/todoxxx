69f74a59b2e9cd639d87eff5b3bb889d5ec2fb94
haloapping <alfiyanto.kondolele@gmail.com>
2025-11-23 20:48:01 +0700

    feat: add request validation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
-from scalar_fastapi import get_scalar_api_reference
 import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from scalar_fastapi import get_scalar_api_reference
 
 from task import task_router
 from user import user_router
@@ -8,6 +11,14 @@ from user import user_router
 app = FastAPI(summary="Todo App", description="Todo App")
 app.include_router(user_router)
 app.include_router(task_router)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content=jsonable_encoder({"error": exc.errors(), "body": exc.body}),
+    )
 
 
 @app.get("/", include_in_schema=False)
